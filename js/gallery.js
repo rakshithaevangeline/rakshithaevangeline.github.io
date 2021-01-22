2b6abd92ff23615a73f9105fa7f33cc0023071a0
Rakshitha Evangeline <rakshithaevangeline29@gmail.com>
2021-01-22 16:40:46 +0530

    Add code to change the main image, title and description with js

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,16 @@
+
 function activateGallery() {
-  alert("hello");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs div img");
+  let mainImage = document.querySelector("#gallery-photo img");
+  let imageTitle = document.querySelector("#gallery-info h3.title");
+  let imageDescription = document.querySelector("#gallery-info p.description");
+
+
+  thumbnails.forEach(thumbnail => {
+    thumbnail.addEventListener("click", function() {
+      mainImage.setAttribute("src", thumbnail.dataset.largeVersion);
+      imageTitle.innerHTML = thumbnail.dataset.title;
+      imageDescription.innerHTML = thumbnail.dataset.description;
+    });
+  });
 }
