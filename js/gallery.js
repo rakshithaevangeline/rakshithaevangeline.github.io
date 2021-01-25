cf67ec86028ed218650c851ed36901d0dda8bc5d
Rakshitha Evangeline <rakshithaevangeline29@gmail.com>
2021-01-25 16:03:36 +0530

    Add code to make the clicked image current

--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,7 @@
 
 function activateGallery() {
   let thumbnails = document.querySelectorAll("#gallery-thumbs div img");
+
   let mainImage = document.querySelector("#gallery-photo img");
   let imageTitle = document.querySelector("#gallery-info h3.title");
   let imageDescription = document.querySelector("#gallery-info p.description");
@@ -8,9 +9,15 @@ function activateGallery() {
 
   thumbnails.forEach(thumbnail => {
     thumbnail.addEventListener("click", function() {
+
+      // Set clicked image as the main image
       mainImage.setAttribute("src", thumbnail.dataset.largeVersion);
       imageTitle.innerHTML = thumbnail.dataset.title;
       imageDescription.innerHTML = thumbnail.dataset.description;
+
+      // Set clicked image as current.
+      document.querySelector(".current").removeAttribute("class");
+      thumbnail.parentElement.setAttribute("class", "current");
     });
   });
 }
