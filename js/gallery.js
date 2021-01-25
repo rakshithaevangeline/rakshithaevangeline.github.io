d75f9190bda960965f9730c297bbe65aab47a001
Rakshitha Evangeline <rakshithaevangeline29@gmail.com>
2021-01-25 16:41:47 +0530

    More tweaking

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,11 @@
+// Activates the image gallery
+// The main task is to set a event listener for each image and set it to behave 
+// appropriately.
 
 function activateGallery() {
   let thumbnails = document.querySelectorAll("#gallery-thumbs div img");
-
   let mainImage = document.querySelector("#gallery-photo img");
-  let imageTitle = document.querySelector("#gallery-info h3.title");
+   let imageTitle = document.querySelector("#gallery-info h3.title");
   let imageDescription = document.querySelector("#gallery-info p.description");
 
 
@@ -12,12 +14,15 @@ function activateGallery() {
 
       // Set clicked image as the main image
       mainImage.setAttribute("src", thumbnail.dataset.largeVersion);
-      imageTitle.innerHTML = thumbnail.dataset.title;
-      imageDescription.innerHTML = thumbnail.dataset.description;
-
+      mainImage.setAttribute("alt", thumbnail.getAttribute("alt"));
+      
       // Set clicked image as current.
       document.querySelector(".current").removeAttribute("class");
       thumbnail.parentElement.setAttribute("class", "current");
+
+      // Set image title and description to match the clicked image
+      imageTitle.innerHTML = thumbnail.dataset.title;
+      imageDescription.innerHTML = thumbnail.dataset.description;
     });
   });
 }
